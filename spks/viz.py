c685b8c659cea4068f4264b0012c32e1b9370f78
Max Melin <mmelin@g.ucla.edu>
2023-08-22 11:50:21 -0700

    plot event aligned rasters

--- spks/viz.py
+++ spks/viz.py
@@ -29,6 +29,74 @@ def plot_raster(spks,offset=0.0,height=1.0,colors='black',ax = None,**kwargs):
                   offset+((i+1)*height),
                   colors=cc,**kwargs)
 
+def plot_event_aligned_raster(event_times, spike_times, sorting='', pre_seconds=1, post_seconds=2, offset=0, ax=None, color='black', **kwargs):
+    """Plot rasters from multiple trials aligned to event_times
+
+    Parameters
+    ----------
+    event_times : list or ndarray
+        a list or numpy array of event times to be aligned to
+    spike_times : list or ndarray
+        a list of all spike times for one cluster
+    sorting : _type_, optional
+        _description_, by default None
+    pre_seconds : int, optional
+        grab _ seconds before event_times for alignment, by default 1
+    post_seconds : int, optional
+        grab _ seconds after event_times for alignment, by default 2
+    ax : _type_, optional
+        matplotlib axes object to plot on, by default None and will create an axis
+    color : str, optional
+        plotting color, by default 'black'
+    """    
+    #TODO: add subsample option
+    event_times = discard_nans(event_times)
+    if ax is None:
+        ax = plt.gca()
+    n_events = len(event_times)
+
+    if sorting == 'time_to_first_spike':
+        first_spike_deltas = []
+        for event_time in event_times:
+            relative_spiketimes = spike_times - event_time
+            first_spike_deltas.append(next(time for time in relative_spiketimes if time > 0))
+        event_times = event_times[np.argsort(first_spike_deltas)]
+        ax.set_ylabel('Sorted by time to first spike')
+
+    rasters = rasterize_to_event(event_times, spike_times, pre_seconds, post_seconds)
+    for i, event_raster in enumerate(rasters):
+        ax.vlines(event_raster,(offset+i),
+                  (offset+i+1), color=color, **kwargs)
+    ax.set_xlabel('Time relative to event (s)')
+    ax.set_ylim(ax.get_ylim()[::-1]) # flip the y axis
+
+def plot_event_based_raster_fast(event_times, spike_times, pre_seconds=1, post_seconds=2, offset=0, ax=None, color='black', **kwargs):
+    """faster version of plot_event_aligned_raster for SpikeViewer"""
+    #TODO: add subsample option
+    event_times = discard_nans(event_times)
+    if ax is None:
+        ax = plt.gca()
+    n_events = len(event_times)
+
+    rasters = rasterize_to_event(event_times, spike_times, pre_seconds, post_seconds)
+    x = []
+    ymin =[] 
+    ymax = []
+    for i, event_raster in enumerate(rasters):
+        x.extend(event_raster)
+        ymin.extend([offset+i]*len(event_raster))
+        ymax.extend([offset+i+1]*len(event_raster))
+    x = np.array(x)
+    ymin = np.array(ymin)
+    ymax = np.array(ymax)
+    #ax.vlines(x, ymin, ymax, color=color, **kwargs)
+    ax.scatter(x, ymin, s=.2, linewidths=.2, marker='|', color=color, **kwargs) #TODO: adaptive s and linewidth kwargs
+    #ax.scatter(x, ymin, s=5, linewidths=5, marker='|', color=color, **kwargs)
+    ax.set_xlabel('Time relative to event (s)')
+    #ax.set_ylim((np.min(ymin),np.max(ymax)))
+    #ax.set_xlim((np.min(x), np.max(x)))
+    #ax.set_ylim(ax.get_ylim()[::-1]) # flip the y axis
+
 
 def plot_multichannel_data(dat, chorder, srate = 30000.,
                            offset = 1000, filterdata = False,
